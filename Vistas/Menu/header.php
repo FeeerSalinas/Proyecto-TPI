2726c49e956668dcd580e5d1e23474d6f6bc721a
Fatima Alexandra Castillo Martinez <cm17102@ti.ues.edu.sv>
2024-10-21 21:24:30 -0600

    Menu css

--- Vistas/Menu/header.php
+++ Vistas/Menu/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Freelancer</title>
-    <link rel="stylesheet" href="/Proyecto-TPI/CSS/menu.css">
+    <link rel="stylesheet" href="../../CSS/menu.css">
     <!-- Bootstrap CSS -->
     <link 
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
--- Vistas/Menu/navbarContratista.php
+++ Vistas/Menu/navbarContratista.php
@@ -10,15 +10,15 @@
 
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#">Buscar Proyectos</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Mis Proyectos</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Mis propuestas</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Buscar Freelncers</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Crear Proyecto</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Mis proyectos</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Mi Perfil</a></li>
             </ul>
 
             <!-- Avatar de Usuario -->
             <div class="d-flex align-items-center">
-                <img src="/Proyecto-TPI/IMG/user.png" alt="Usuario" class="rounded-circle" height="40">
+                <img src="../../IMG/user.png" alt="Usuario" class="rounded-circle" height="40">
             </div>
         </div>
     </div>
