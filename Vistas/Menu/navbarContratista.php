0e91b493d62a5a836cfc6e8734534c60c9dd5367
Fatima Alexandra Castillo Martinez <cm17102@ti.ues.edu.sv>
2024-10-22 23:32:06 -0600

    Revert "boton logout en perfil"
    This reverts commit 92b533cd072dcabe673bc22d227cf5dcf95d3344.

--- Vistas/Menu/navbarContratista.php
+++ Vistas/Menu/navbarContratista.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
         <!-- Botón de Menú con Estilo Personalizado -->
@@ -17,43 +16,10 @@
                 <li class="nav-item"><a class="nav-link" href="#">Mi Perfil</a></li>
             </ul>
 
-             <!-- Avatar con Menú Desplegable -->
-             <div class="dropdown">
-                <a 
-                    href="#" 
-                    class="d-flex align-items-center text-decoration-none dropdown-toggle" 
-                    id="dropdownUser" 
-                    data-bs-toggle="dropdown" 
-                    aria-expanded="false"
-                >
-                    <img 
-                        src="../../IMG/user.png" 
-                        alt="Usuario" 
-                        class="rounded-circle perfil-img" 
-                        height="40"
-                    >
-                    <span class="nombre-usuario ms-3 text-dark">
-                        <?= htmlspecialchars($_SESSION['nombreUsuario']) ?>
-                    </span>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
-                    <li><a class="dropdown-item" href="#">Mi Perfil</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="../../logout.php">Cerrar Sesión</a></li>
-                </ul>
+            <!-- Avatar de Usuario -->
+            <div class="d-flex align-items-center">
+                <img src="../../IMG/user.png" alt="Usuario" class="rounded-circle" height="40">
             </div>
         </div>
     </div>
 </nav>
-
-<!-- Bootstrap JS y Popper.js -->
-<script 
-    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" 
-    crossorigin="anonymous">
-</script>
-<script 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" 
-    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" 
-    crossorigin="anonymous">
-</script>
