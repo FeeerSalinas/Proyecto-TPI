920802c5ee0255a48dfe8dbca28a71a17308707f
Fatima Alexandra Castillo Martinez <cm17102@ti.ues.edu.sv>
2024-10-26 12:03:57 -0600

    correcciones de merge con rama Fer

--- Vistas/Menu/navbarContratista.php
+++ Vistas/Menu/navbarContratista.php
@@ -10,7 +10,7 @@
 
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="#">Buscar Freelncers</a></li>
+                <li class="nav-item"><a class="nav-link" href="..\Contratista\BuscarFreelancer.php">Buscar Freelancers</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Crear Proyecto</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Mis proyectos</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Mi Perfil</a></li>
